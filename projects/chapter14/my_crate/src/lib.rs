53f755b03d45b550c121f9d4ce4a27e13d32699e
han2time <rlagks0429@gmail.com>
2021-09-26 15:22:34 +0900

    chapter14

---
+++ projects/chapter14/my_crate/src/lib.rs
@@ -0,0 +1,18 @@
+//! # My Crate
+//! 
+//! `my_crate`는 일부 연산을 더 쉽게 실행하기 위한 유틸리티 모음이다.
+
+/// 주어진 숫자에 1을 더한다
+/// 
+/// # Example
+/// 
+/// ```
+/// let arg = 5;
+/// let answer = my_crate::add_one(arg);
+/// 
+/// assert_eq!(6, answer);
+/// ```
+
+pub fn add_one(x: i32) -> i32 {
+    x +1
+}
